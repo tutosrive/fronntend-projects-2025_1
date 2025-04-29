999efe8adc5a3cce62e4f1f3d949b4241301dacd
tutosrive <tutosrivegamer@gmail.com>
2025-04-28 22:03:25 -0500

    Add class 28-04-2025, first Angular project

---
+++ frameworks/angular/demo-angular1/src/app/pages/seats/seats.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SeatsRoutingModule } from './seats-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SeatsRoutingModule
+  ]
+})
+export class SeatsModule { }
